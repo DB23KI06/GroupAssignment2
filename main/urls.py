8af51428ffb47d13f75a6c7fb868fb6acb79c2bb
Revaldyhfz <revaldyhafiz16@gmail.com>
2023-11-22 23:18:46 +0700

    1/3

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, login, register, adminregister, childregister, caregiverregister, driverregister, userdashboard
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('adminregister/', adminregister, name='adminregister'),
+    path('childregister/', childregister, name='childregister'),
+    path('caregiverregister/', caregiverregister, name='caregiverregister'),
+    path('driverregister/', driverregister, name='driverregister'),
+    path('userdashboard/', userdashboard, name="userdashboard")
+    
 ]
--- main/views.py
+++ main/views.py
@@ -4,8 +4,48 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
     }
 
     return render(request, 'main.html', context)
+
+def login(request):
+    context = {
+    }
+
+    return render(request, 'login.html', context)
+
+def register(request):
+    context = {
+    }
+
+    return render(request, 'register.html', context)
+
+def adminregister(request):
+    context = {
+    }
+
+    return render(request, 'adminregister.html', context)
+
+def childregister(request):
+    context = {
+    }
+
+    return render(request, 'childregister.html', context)
+
+def caregiverregister(request):
+    context = {
+    }
+
+    return render(request, 'caregiverregister.html', context)
+
+def driverregister(request):
+    context = {
+    }
+
+    return render(request, 'driverregister.html', context)
+
+def userdashboard(request):
+    context = {
+    }
+
+    return render(request, 'userdashboard.html', context)
