06ddb59d4771bfa8586241b89c5e20e607231054
Revaldyhfz <revaldyhafiz16@gmail.com>
2023-11-26 20:29:07 +0700

    finished frontends

--- main/views.py
+++ main/views.py
@@ -49,3 +49,147 @@ def userdashboard(request):
     }
 
     return render(request, 'userdashboard.html', context)
+
+def admindashboard(request):
+    context = {
+    }
+
+    return render(request, 'admindashboard.html', context)
+
+def dailyreport(request):
+    context = {
+    }
+
+    return render(request, 'dailyreport.html', context)
+
+def enrollment(request):
+    context = {
+    }
+
+    return render(request, 'enrollment.html', context)
+
+def payment(request):
+    context = {
+    }
+
+    return render(request, 'payment.html', context)
+
+def paymenthistory(request):
+    context = {
+    }
+
+    return render(request, 'paymenthistory.html', context)
+
+def programs(request):
+    context = {
+    }
+
+    return render(request, 'programs.html', context)
+
+def programform(request):
+    context = {
+    }
+
+    return render(request, 'programform.html', context)
+
+def updateprogram(request):
+    context = {
+    }
+
+    return render(request, 'updateprogram.html', context)
+
+def offeredprogram(request):
+    context = {
+    }
+
+    return render(request, 'offeredprogram.html', context)
+
+def newprogramoffer(request):
+    context = {
+    }
+
+    return render(request, 'newprogramoffer.html', context)
+
+def offeredprogramdetail(request):
+    context = {
+    }
+
+    return render(request, 'offeredprogramdetail.html', context)
+
+def addmenuschedule(request):
+    context = {
+    }
+
+    return render(request, 'addmenuschedule.html', context)
+
+def extracurricular(request):
+    context = {
+    }
+
+    return render(request, 'extracurricular.html', context)
+
+def extracurriculardetail(request):
+    context = {
+    }
+
+    return render(request, 'extracurriculardetail.html', context)
+
+def extracurricularform(request):
+    context = {
+    }
+
+    return render(request, 'extracurricularform.html', context)
+
+def activity(request):
+    context = {
+    }
+
+    return render(request, 'activity.html', context)
+
+def activityform(request):
+    context = {
+    }
+
+    return render(request, 'activityform.html', context)
+
+def updateactivity(request):
+    context = {
+    }
+
+    return render(request, 'updateactivity.html', context)
+
+def menu(request):
+    context = {
+    }
+
+    return render(request, 'menu.html', context)
+
+def menuform(request):
+    context = {
+    }
+
+    return render(request, 'menuform.html', context)
+
+def updatemenu(request):
+    context = {
+    }
+
+    return render(request, 'updatemenu.html', context)
+
+def room(request):
+    context = {
+    }
+
+    return render(request, 'room.html', context)
+
+def roomform(request):
+    context = {
+    }
+
+    return render(request, 'roomform.html', context)
+
+def paymenthistory(request):
+    context = {
+    }
+
+    return render(request, 'paymenthistory.html', context)
